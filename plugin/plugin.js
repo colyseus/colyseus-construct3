2c298bd721a7beb4e8fab0f99e58a657814670d3
Endel Dreyer <endel.dreyer@gmail.com>
2020-10-15 16:14:07 -0300

    update colyseus.js to v0.14

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -12,7 +12,7 @@
   const PLUGIN_ID = "Colyseus";
   ////////////////////////////////////////////
 
-  const PLUGIN_VERSION = "0.13.3.0";
+  const PLUGIN_VERSION = "0.14.0.0";
   const PLUGIN_CATEGORY = "web";
   const PLUGIN_AUTHOR = "Endel Dreyer";
 
